55b33c3b178a06fcfeec2d70395479a96d4968b0
tkaczmarz <kaczmarz.tomasz@gmail.com>
2018-06-09 11:50:51 +0200

    Added points

---
+++ Komiwojazer/Point.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Komiwojazer
+{
+    class Point
+    {
+        public double x;
+        public double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static List<Point> GetPoints(params double[] coords)
+        {
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < coords.Length - 1; i += 2)
+            {
+                points.Add(new Point(coords[i], coords[i + 1]));
+            }
+            return points;
+        }
+    }
+}
--- Komiwojazer/Program.cs
+++ Komiwojazer/Program.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Komiwojazer
 {
-    /*
-     * Program ze zbioru punktów ma dać na wyjściu zamkniętą ścieżkę bitoniczną.
-     */
     class Program
     {
         static void Main(string[] args)
         {
+            List<Point> points = Point.GetPoints(-2, -5, 0, 4, 2, 3, 1, 7);
         }
     }
 }
