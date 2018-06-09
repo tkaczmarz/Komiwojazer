bcebaf97e0c6299d28b1c11fb70c603a432a4bca
tkaczmarz <kaczmarz.tomasz@gmail.com>
2018-06-09 11:01:16 +0200

    Init

---
+++ Komiwojazer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komiwojazer
+{
+    /*
+     * Program ze zbioru punktów ma dać na wyjściu zamkniętą ścieżkę bitoniczną.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
